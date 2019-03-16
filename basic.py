6787aceaa9ea28b2573d587f3cf2e47866876c98
saujanya01 <saujanyatiwari455@gmail.com>
2019-03-16 19:44:12 +0530

    One

---
+++ basic.py
@@ -0,0 +1,6 @@
+from tkinter import *
+
+root = Tk()# displays main window
+label = Label(root , text="Emma watson")#for some text in the window
+label.pack()#'.pack(), is for random positioning of the text label
+root.mainloop()#a loop that runs till the close button in pressed
